b09119b3167ed75ba45e3995b1545325b2c6bd84
Federico Joaquin <federico.joaquin@cs.uns.edu.ar>
2023-10-02 14:35:04 -0300

    Modifica todas las imagenes asociadas a los caramelos, y los valores que representan sus colores.

--- src/Logica/Color.java
+++ src/Logica/Color.java
@@ -6,12 +6,10 @@ package Logica;
  *
  */
 public class Color {
-	public static final int VERDE = 1;
-	public static final int LADRILLO = 2;
+	public static final int AZUL = 1;
+	public static final int VERDE = 2;
 	public static final int NARANJA = 3;
-	public static final int AMARILLO = 4;
-	public static final int AZUL = 5;
-	public static final int VIOLETA = 6;
-	public static final int CELESTE = 7;
-	public static final int TRANSPARENTE = 8;
+	public static final int VIOLETA = 4;
+	public static final int ROJO = 5;
+	public static final int NEGRO = 6;
 }
